5a00f9f4f5fdab0d37932cab471c664ff57cd30c
Sanjith S <sanjithsece@gmail.com>
2025-07-15 06:44:02 +0530

    test

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM maven:3.8.5-openjdk-17 AS build
 WORKDIR /app
 COPY springboot-ems-master /app
 RUN mvn clean package -DskipTests
-
 FROM openjdk:17.0.1-jdk-slim
 WORKDIR /app
 COPY --from=build /app/target/*.jar springbootfirst.jar
---
+++ springboot-ems-master/src/main/java/com/example/springbootfirst/controllers/Hello.java
@@ -0,0 +1,9 @@
+package com.example.springbootfirst.controllers;
+
+public class Hello {
+    public String helloTest() {
+        System.out.println("test in Hello controller");
+        String data = "finish";
+        return data;
+    }
+}
