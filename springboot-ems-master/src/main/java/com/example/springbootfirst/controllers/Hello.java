37bfbfcb7813166fac09e0125082a9ef42b9edfd
Sanjith S <sanjithsece@gmail.com>
2025-07-15 10:11:38 +0530

    Updated Hello.java file

--- springboot-ems-master/src/main/java/com/example/springbootfirst/controllers/Hello.java
+++ springboot-ems-master/src/main/java/com/example/springbootfirst/controllers/Hello.java
@@ -3,7 +3,7 @@ package com.example.springbootfirst.controllers;
 public class Hello {
     public String helloTest() {
         System.out.println("test in Hello controller");
-        String data = "finish";
-        return data;
+        String info = "finish";
+        return info;
     }
 }
